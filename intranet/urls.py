dae094f07ed8c025216eac1dbfe440ba103ff044
Jay-68 <kelvinngari1995@gmail.com>
2020-01-11 21:23:14 +0300

    create a holder updates view and its template

--- intranet/urls.py
+++ intranet/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 
 urlpatterns = [
-    
+    path('updates',views.updates,name='updates'),
 ]
-
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
