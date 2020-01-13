874ddc09befc6d6a6e13af04190f3368d80ff3ba
danielevans-999 <danielevans.karani@gmail.comgit>
2020-01-13 14:43:17 +0300

    Adds the custom auth

--- pawame/settings.py
+++ pawame/settings.py
@@ -114,7 +114,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+# custom authentications
+AUTH_USER_MODEL = 'intranet.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
