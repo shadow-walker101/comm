65be1815e6dfdabc086d4efbb9f9833ae5fc8a4a
JMOS_MBUGUA <johnmbugua849@gmail.com>
2020-01-13 10:57:07 +0300

    difine function notifications

--- intranet/views.py
+++ intranet/views.py
@@ -5,3 +5,5 @@ from django.shortcuts import render
 
 def updates(request):
     return render(request, 'updates.html')
+def notifications(request):
+    return render(request, 'notifications.html')
