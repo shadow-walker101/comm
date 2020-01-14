b223d54142c331a0e53b17e61c6b59c8c4922570
dicksonkariuki <dicksonkariuki4@gmail.com>
2020-01-14 18:52:40 +0300

    Modify url for information_technology view

--- intranet/urls.py
+++ intranet/urls.py
@@ -9,12 +9,10 @@ urlpatterns = [
     path('notifications', views.notifications, name='notifications'),
     path('departments', views.departments, name='departments'),
     path('employees', views.employees, name='employees'),
-
     path('inventory', views.inventory, name='inventory'),
     path('finance', views.finance, name='finance'),
     path('marketing', views.marketing, name='marketing'),
-    path('information', views.information, name='information'),
-
+    path('information', views.information_technology, name='information'),
     path('employeeProfile', views.employeeProfile, name='employeeProfile')
 ]
 
