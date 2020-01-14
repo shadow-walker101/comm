832f2ba176b1cfd17e9d26df73998d982568670c
RKNyra <rk.nyra@gmail.com>
2020-01-14 14:58:27 +0300

    create authorization for the employees page such that a user is required to log in to view the employees page

--- intranet/views.py
+++ intranet/views.py
@@ -1,18 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.contrib.auth import login, authenticate
+from . models import *
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import REDIRECT_FIELD_NAME
 
-# Create your views here.
+
 def departments(request):
     return render(request, 'department.html')
-
-
 
 def updates(request):
     return render(request, 'updates.html')
 
-
-
+@login_required(login_url='accounts/login')
 def employees(request):
     return render(request, 'employees.html')
 
 def notifications(request):
-    return render(request, 'notifications.html
+    return render(request, 'notifications.html')
