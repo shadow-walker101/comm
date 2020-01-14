40dd8faa14226f13f437564c67fe03568e34f4c4
danielevans-999 <danielevans.karani@gmail.comgit>
2020-01-14 18:51:08 +0300

    Adds the login view

--- intranet/views.py
+++ intranet/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect, HttpResponse, HttpResponseRedirect
+from django.contrib.auth import login, authenticate
+from . models import * 
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import REDIRECT_FIELD_NAME
 
-# Create your views here.
+
+def login (request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('updates')
+        
 def departments(request):
     return render(request, 'department.html')
 
@@ -10,7 +26,7 @@ def updates(request):
     return render(request, 'updates.html')
 
 
-
+@user_passes_test(lambda u:u.is_active , login_url='/accounts/login')
 def employees(request):
     return render(request, 'employees.html')
 
