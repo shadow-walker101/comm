a088ec543a06975a724d07d90fe96c40c1e4fc2f
dicksonkariuki <dicksonkariuki4@gmail.com>
2020-01-14 18:04:06 +0300

    Modify the urls to show notifications as the homepage

--- intranet/urls.py
+++ intranet/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+from .import views
 
 
 urlpatterns = [
 
-    path('', views.departments, name='departments'),
+    path('', views.notifications, name='notifications'),
     path('updates', views.updates, name='updates'),
     path('employees', views.employees, name='employees')
 ]
--- intranet/views.py
+++ intranet/views.py
@@ -6,24 +6,27 @@ from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import REDIRECT_FIELD_NAME
 
-@user_passes_test(lambda u:u.is_active and u.department=4,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
+@user_passes_test(lambda u: u.is_active and u.department==4,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
 def marketing(request):
     template='marketing.html'
     return render(request, template)
 
-@user_passes_test(lambda u:u.is_active and u.department=1,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
+@user_passes_test(lambda u: u.is_active and u.department==1,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
 def human_resource(request):
     template='human_resource.html'
     return render(request,template)
-@user_passes_test(lambda u:u.is_active and u.department=3,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
+
+@user_passes_test(lambda u:u.is_active and u.department==3,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
 def finance(request):
     template='finance.html'
     return render(request,template)
-@user_passes_test(lambda u:u.is_active and u.department=2,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
+
+@user_passes_test(lambda u:u.is_active and u.department==2,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
 def inventory(request):
     template='inventory.html'
     return render(request,template)
-@user_passes_test(lambda u:u.is_active and u.department=5,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
+
+@user_passes_test(lambda u:u.is_active and u.department==5,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
 def information_technology(request):
     template='information_technology.html'
     return render(request,template)
