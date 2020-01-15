729a678abcb95b6ca4541e737d7ac61878909468
RKNyra <rk.nyra@gmail.com>
2020-01-15 17:58:25 +0300

    create the post-update page from where super-admins and admins can share information

--- intranet/urls.py
+++ intranet/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('information', views.information_technology, name='information'),
     path('employeeProfile', views.employeeProfile, name='employeeProfile'),
     path('human_resource',views.human_resource,name='human_resource'),
+    path('postUpdate',views.postUpdate,name='postUpdate'),
 ]
