1fced2bcb226ebbd38ea9634a52ea6d66ea09c61
danielevans-999 <danielevans.karani@gmail.comgit>
2020-01-16 02:52:01 +0300

    Adds the updates form

--- intranet/forms.py
+++ intranet/forms.py
@@ -1,17 +1,5 @@
 from django import forms
 from .models import *
-from django.auth.forms import UserCreationForm
-
-class UserRegistrationForm(UserCreationForm)
-username=forms.CharField(max_length=30)
-email=forms.EmailField(max_length=200)
-
-class meta:
-    model=User
-    fields=('username','email','user_type','departments','employee_id')
-
-class LoginForm()
-
 class PostUpdateForm(forms.ModelForm):
     class Meta:
         model = Updates
