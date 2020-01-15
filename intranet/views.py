e9441bb9afa47559d2026c16ea9d73d69851d781
daisyarusey <daisyarusey.dc@gmail.com>
2020-01-15 17:56:49 +0300

    Merge

--- intranet/views.py
+++ intranet/views.py
@@ -1,4 +1,5 @@
 
+
 from django.shortcuts import render , redirect, HttpResponse, HttpResponseRedirect
 from django.contrib.auth import login, authenticate
 from . models import * 
@@ -18,9 +19,10 @@ def login (request):
             login(request, user)
             return redirect('updates')
         
+
 def departments(request):
     return render(request, 'department.html')
-@user_passes_test(lambda u: u.is_active and u.department==4,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
+# @user_passes_test(lambda u: u.is_active and u.department==4,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
 def marketing(request):
     template='marketing.html'
     return render(request, template)
@@ -30,19 +32,24 @@ def human_resource(request):
     template='human_resource.html'
     return render(request,template)
 
-@user_passes_test(lambda u:u.is_active and u.department==3,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
+
+def updates(request):
+    return render(request, 'updates.html')
+
+# @user_passes_test(lambda u:u.is_active and u.department==3,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
 def finance(request):
     template='finance.html'
     return render(request,template)
 
-@user_passes_test(lambda u:u.is_active and u.department==2,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
+# @user_passes_test(lambda u:u.is_active and u.department==2,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
 def inventory(request):
     template='inventory.html'
     return render(request,template)
 
-@user_passes_test(lambda u:u.is_active and u.department==5,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
+
+# @user_passes_test(lambda u:u.is_active and u.department==5,redirect_field_name=REDIRECT_FIELD_NAME,login_url='account/login')
 def information_technology(request):
-    template='information_technology'
+    template='information_technology.html'
     return render(request,template)
 
 def departments(request):
@@ -53,20 +60,28 @@ def updates(request):
     return render(request,template)
 
 @login_required(login_url='accounts/login')
-def employees(request):
-    template='employees.html'
-    return render(request, template)
 
-def notifications(request):
-    template='notifications.html'
-    return render(request, template)
+def employees(request):
+
+    if request.user.user_type == 1 or request.user.user_type == 2:
+        return render(request, 'employees.html')
+    else:
+        return render(request, 'employeeProfile.html')
 
 
 def login(request):
     return render(request, 'login.html')
    
 @login_required(login_url='accounts/login')
+def notifications(request):
+
+    return render(request, 'notifications.html')
+
 def employeeProfile(request):
-    template='employeeProfile.html'
+    return render(request, 'employeeProfile.html')
+
+    template='notifications.html'
     return render(request, template)
+
+
 
