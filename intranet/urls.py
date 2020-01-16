2de5687a723340d2b31b736710d8af74bfff15d6
RKNyra <rk.nyra@gmail.com>
2020-01-16 18:27:41 +0300

    ...

--- intranet/urls.py
+++ intranet/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
--- pawame/settings.py
+++ pawame/settings.py
@@ -165,5 +165,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = ''
