91745fdc679a9fc2d02db6951edb9cfc2a314215
danielevans-999 <danielevans.karani@gmail.comgit>
2020-01-19 23:19:38 +0300

    Rearranges the view according to the departments order

--- intranet/views.py
+++ intranet/views.py
@@ -41,32 +41,34 @@ def updates(request):
     users = User.objects.order_by('-last_login')
     return render(request, 'updates.html' ,{'updates':updates, 'users':users})
 
-def marketing(request):
-    template='marketing.html'
-    updates = Updates.objects.filter(department=5).all()
-    return render(request, template,{'updates':updates})
 
-@user_passes_test(lambda u: u.is_active and u.department==1 and u.user_type==1 ,redirect_field_name=REDIRECT_FIELD_NAME,login_url='login')
+@user_passes_test(lambda u: u.is_active and u.department==1 or u.user_type==1 ,redirect_field_name=REDIRECT_FIELD_NAME,login_url='login')
 def human_resource(request):
     template='human_resource.html'
     updates = Updates.objects.filter(department=2).all()
     return render(request,template, {'updates':updates})
 
-
-@user_passes_test(lambda u:u.is_active and u.department==3,redirect_field_name=REDIRECT_FIELD_NAME,login_url='login')
-def finance(request):
-    template='finance.html'
-    updates = Updates.objects.filter(department=6).all()
-    return render(request,template,{'update':updates})
-
-@user_passes_test(lambda u:u.is_active and u.department==2,redirect_field_name=REDIRECT_FIELD_NAME,login_url='login')
+@user_passes_test(lambda u:u.is_active and u.department==2 or u.user_type==1,redirect_field_name=REDIRECT_FIELD_NAME,login_url='login')
 def inventory(request):
     template='inventory.html'
     updates = Updates.objects.filter(department=4).all()
     return render(request,template,{'update':updates})
 
 
-@user_passes_test(lambda u:u.is_active and u.department==5,redirect_field_name=REDIRECT_FIELD_NAME,login_url='login')
+
+@user_passes_test(lambda u:u.is_active and u.department==3 or u.user_type==1,redirect_field_name=REDIRECT_FIELD_NAME,login_url='login')
+def finance(request):
+    template='finance.html'
+    updates = Updates.objects.filter(department=6).all()
+    return render(request,template,{'update':updates})
+
+@user_passes_test(lambda u:u.is_active and u.department==4 or u.user_type==1,redirect_field_name=REDIRECT_FIELD_NAME,login_url='login')
+def marketing(request):
+    template='marketing.html'
+    updates = Updates.objects.filter(department=5).all()
+    return render(request, template,{'updates':updates})
+
+@user_passes_test(lambda u:u.is_active and u.department==5 or u.user_type==1,redirect_field_name=REDIRECT_FIELD_NAME,login_url='login')
 def information_technology(request):
     template='information_technology.html'
     updates = Updates.objects.filter(department=3).all()
