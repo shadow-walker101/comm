d98dccb63bbe26bc9a391e1682339b8628bab126
danielevans-999 <danielevans.karani@gmail.comgit>
2020-01-23 11:49:55 +0300

    Adds a status field in the updates model

--- intranet/models.py
+++ intranet/models.py
@@ -73,6 +73,11 @@ class User(AbstractBaseUser, PermissionsMixin):
     def has_module_perms(self, app_label):
         return True
     
+    @classmethod
+    def search_employees(cls,employee):
+        employee = cls.objects.filter(username__icontains = employee)
+        return employee
+    
     @property
     def is_staff(self):
         return self.is_admin
@@ -118,10 +123,13 @@ class Updates(models.Model):
     time_stamp = models.DateTimeField(auto_now=True) 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     department =  models.PositiveSmallIntegerField(choices=UPDATE_TYPES, null=True)
+    status = models.BooleanField(default=False)
     
     @classmethod
     def get_update(cls,id):
         update = get_object_or_404(cls, pk=id) 
+    
+    
         
     def __str__(self):
           return self.title
