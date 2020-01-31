6ad5cb9ff3a18e6bf77968ed32c0cf6556ccbf28
danielevans-999 <danielevans.karani@gmail.comgit>
2020-01-31 06:16:54 +0300

    Adds password reset templates

--- intranet/forms.py
+++ intranet/forms.py
@@ -37,17 +37,17 @@ class CommentForm(forms.ModelForm):
         
 class UserCreationForm(forms.ModelForm):
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput, required=False)
-    password2 = forms.CharField(
-        label='Password confirmation', widget=forms.PasswordInput, required=False)
+    # password2 = forms.CharField(
+    #     label='Password confirmation', widget=forms.PasswordInput, required=False)
     class Meta:
         model = User
         fields = ('email', 'user_type', 'department', 'username')
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
+    # def clean_password2(self):
+    #     password1 = self.cleaned_data.get("password1")
+    #     password2 = self.cleaned_data.get("password2")
+    #     if password1 and password2 and password1 != password2:
+    #         raise forms.ValidationError("Passwords don't match")
+    #     return password2
     
     def save(self, commit=True):
         user = super().save(commit=False)
--- pawame/urls.py
+++ pawame/urls.py
@@ -47,5 +47,8 @@ urlpatterns = [
     path('', include('intranet.urls')),
     path('tinymce/', include('tinymce.urls')),
     path('summernote/', include('django_summernote.urls')),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     # path('accounts/',include('authtools.urls')),
 ]
